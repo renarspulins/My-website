1181c4c6ff9e43ce494811fbfa9a7df9aff233e6
renarspulins <renars.pulins@gmail.com>
2024-02-22 10:14:35 +0200

    otrais

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // script.js
 
-document.getElementById("lebronBox").addEventListener("click", function() {
-    window.location.href = "lebron_latvian.html";
-});
+// Function to handle box click event and redirect to player's subpage
+function goToPlayerPage(playerId) {
+    window.location.href = `player${playerId}.html`;
+}
